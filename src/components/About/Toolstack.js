dad264385a8389dcc58bcfd8b91b1d4fa7d39e6a
Freelanceprojects6 <rockyninganiya@gmail.com>
2024-11-15 17:18:02 +0530

    Update Toolstack.js
    figma and blender tools added

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -1,4 +1,4 @@
-import React from "react";
+/*import React from "react";
 import { Col, Row } from "react-bootstrap";
 import {
   SiVisualstudiocode,
@@ -26,6 +26,51 @@ function Toolstack() {
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiSlack />
+      </Col>
+    </Row>
+  );
+}
+
+export default Toolstack;
+*/
+
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import {
+  SiVisualstudiocode,
+} from "react-icons/si";
+import {
+  DiMysql,
+  DiJira,
+} from "react-icons/di";
+import { FaFigma, FaBlender } from "react-icons/fa";  // Importing Figma and Blender icons
+
+function Toolstack() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      {/* Visual Studio Code Icon */}
+      <Col xs={4} md={2} className="tech-icons">
+        <SiVisualstudiocode />
+      </Col>
+
+      {/* MySQL Icon */}
+      <Col xs={4} md={2} className="tech-icons">
+        <DiMysql />
+      </Col>
+
+      {/* Jira Icon */}
+      <Col xs={4} md={2} className="tech-icons">
+        <DiJira />
+      </Col>
+
+      {/* Figma Icon */}
+      <Col xs={4} md={2} className="tech-icons">
+        <FaFigma />
+      </Col>
+
+      {/* Blender Icon */}
+      <Col xs={4} md={2} className="tech-icons">
+        <FaBlender />
       </Col>
     </Row>
   );
